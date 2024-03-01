41b15eb68d5c3dca8c3653863b794f4a9b3be27a
roldanm02 <roldan.m@northeastern.edu>
2024-02-29 22:07:11 -0500

    Added service request url to constants

--- constants.py
+++ constants.py
@@ -1,6 +1,6 @@
 import os
-from enum import Enum
 from requests.auth import HTTPBasicAuth
 
 KEY_311 = os.environ.get('311_KEY')
 AUTH_311 = HTTPBasicAuth('key', KEY_311)
+API_URL_311 = 'https://boston2-production.spotmobile.net/open311/v2/services.json'
