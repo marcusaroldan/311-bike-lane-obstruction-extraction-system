3cb0c9674c8bbde8b29874922c13fe7244985820
roldanm02 <roldan.m@northeastern.edu>
2024-03-01 11:16:14 -0500

    Added Illegal Parking service code to constants.py

--- constants.py
+++ constants.py
@@ -1,6 +1,8 @@
 import os
 from requests.auth import HTTPBasicAuth
+from urllib.parse import quote_plus
 
-KEY_311 = os.environ.get('311_KEY')
+KEY_311 = os.environ.get('311_API_KEY')
 AUTH_311 = HTTPBasicAuth('key', KEY_311)
 API_URL_311 = 'https://boston2-production.spotmobile.net/open311/v2/services.json'
+ILLEGAL_PARKING_SERVICE_CODE = quote_plus("Transportation - Traffic Division:Enforcement & Abandoned Vehicles:Parking Enforcement")
